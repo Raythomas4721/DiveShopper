1ae3b73361ead790c86162392ac27d4b32faa158
Ray <ray123321123321@gmail.com>
2024-11-14 16:38:09 +0800

    update member title

--- divingWebProject/View/FrmMember.Designer.cs
+++ divingWebProject/View/FrmMember.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.header1 = new System.Windows.Forms.Label();
+            this.newMember = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // header1
+            // 
+            this.header1.AutoSize = true;
+            this.header1.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.header1.Location = new System.Drawing.Point(39, 55);
+            this.header1.Name = "header1";
+            this.header1.Size = new System.Drawing.Size(123, 35);
+            this.header1.TabIndex = 0;
+            this.header1.Text = "會員列表";
+            // 
+            // newMember
+            // 
+            this.newMember.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.newMember.Location = new System.Drawing.Point(195, 55);
+            this.newMember.Name = "newMember";
+            this.newMember.Size = new System.Drawing.Size(108, 35);
+            this.newMember.TabIndex = 1;
+            this.newMember.Text = "新增會員";
+            this.newMember.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(695, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 33);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(858, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "搜尋";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FrmMember
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(992, 625);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.newMember);
+            this.Controls.Add(this.header1);
             this.Name = "FrmMember";
             this.Text = "FrmMember";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label header1;
+        private System.Windows.Forms.Button newMember;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
