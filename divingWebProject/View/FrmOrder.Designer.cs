f4a99421b36c875384f0758c6dfa6a3b0f4d586f
Chloe <xian19991023@gmail.com>
2024-11-14 11:31:15 +0800

    update order button

--- divingWebProject/View/FrmOrder.Designer.cs
+++ divingWebProject/View/FrmOrder.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(106, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(610, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "新品訂單";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(106, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(610, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "新品訂單";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // FrmOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(890, 679);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "FrmOrder";
             this.Text = "FrmOrder";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -43,5 +67,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
